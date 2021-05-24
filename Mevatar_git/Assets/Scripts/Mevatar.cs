2f7c2ed66a39884cc34295625e796f09811c3061
aprilZheng <zhengtonglin211@gmail.com>
2021-05-24 13:17:32 +0200

    Update Classes

--- Mevatar_git/Assets/Scripts/Mevatar.cs
+++ Mevatar_git/Assets/Scripts/Mevatar.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class Mevatar : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string head;
+    public string eyebrow;
+    public string eyes;
 }
--- Mevatar_git/Assets/Scripts/User.cs
+++ Mevatar_git/Assets/Scripts/User.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class User : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public string email;
+    public string username;
+    public Mevatar mevatar;
 }
